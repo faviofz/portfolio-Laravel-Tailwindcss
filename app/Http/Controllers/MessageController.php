b928e185e73691fc13c8101d6ab2f05268db3f67
Favio Fernandez <faviofz811@gmail.com>
2022-01-17 01:41:09 -0300

    Envio de emails agregado

---
+++ app/Http/Controllers/MessageController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mail\MessageReceived;
+use Illuminate\Support\Facades\Mail;
+
+class MessageController extends Controller
+{
+    public function store()
+    {
+        $data = request()->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'subject' => 'required',
+            'content' => 'required|min:5'
+        ]);
+
+        Mail::to('admin@example.com')->send(new MessageReceived($data));
+
+        return 'Mensaje Enviado';
+    }
+}
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,5 +3,26 @@
 @section('title', 'Contacto')
 
 @section('content')
-<h1>Esta es la pagina de inicio</h1>
+<h1>Contacto</h1>
+<form action="{{ route('contact') }}" method="POST">
+    @csrf
+    <label for="name">
+        Nombre: <br>
+        <input type="text" name="name" id="name"><br>
+    </label>
+    <label for="email">
+        Email: <br>
+        <input type="email" name="email" id="email"><br>
+    </label>
+    <label for="subject">
+        Asunto: <br>
+        <input type="text" name="subject" id="subject"><br>
+    </label>
+    <label for="content">
+        Mensaje: <br>
+        <textarea name="content" id="content" cols="30" rows="10"></textarea><br>
+    </label>
+    <button type="submit">Enviar</button>
+
+</form>
 @endsection
